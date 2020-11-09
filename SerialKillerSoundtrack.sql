a9e620d4fe999c87da92dd2470ffae2c09f95453
Jessica-Thomas <jessica.thomas648@gmail.com>
2020-11-09 17:41:58 -0500

    added final SQL script file

---
+++ SerialKillerSoundtrack.sql
@@ -0,0 +1,41 @@
+﻿--Serial Killers Table--   --Movies Table--   --Soundstrack Table--      --Write a SELECT query that uses a WHERE clause-- 
+
+--Write a  SELECT query that uses an OR and an AND operator-- 
+
+--Write a  SELECT query that filters NULL rows using IS NOT NULL-- 
+
+--Write a DML statement that UPDATEs a set of rows with a WHERE clause. The values used in the WHERE clause should be a variable-- 
+
+--Write a DML statement that DELETEs a set of rows with a WHERE clause. The values used in the WHERE clause should be a variable--
+
+
+--Write a DML statement that DELETEs rows from a table that another table references. This script will have to also DELETE any records that reference these rows. Both of the DELETE statements need to be wrapped in a single TRANSACTION.-- 
+
+--Write a  SELECT query that utilizes a JOIN-- 
+
+--Write a  SELECT query that utilizes a JOIN with 3 or more tables-- 
+
+--Write a  SELECT query that utilizes a LEFT JOIN-- 
+
+--Write a  SELECT query that utilizes a variable in the WHERE clause-- 
+
+--Write a  SELECT query that utilizes a ORDER BY clause-- 
+
+--Write a  SELECT query that utilizes a GROUP BY clause along with an aggregate function-- 
+
+--Write a SELECT query that utilizes a CALCULATED FIELD--
+
+
+--Write a SELECT query that utilizes a SUBQUERY-- 
+
+--Write a SELECT query that utilizes a JOIN, at least 2 OPERATORS (AND, OR, =, IN, BETWEEN, ETC) AND A GROUP BY clause with an aggregate function-- 
+
+--Write a SELECT query that utilizes a JOIN with 3 or more tables, at 2 OPERATORS (AND, OR, =, IN, BETWEEN, ETC), a GROUP BY clause with an aggregate function, and a HAVING clause-- 
+
+--Design a NONCLUSTERED INDEX with ONE KEY COLUMN that improves the performance of one of the above queries-- 
+
+--Design a NONCLUSTERED INDEX with TWO KEY COLUMNS that improves the performance of one of the above queries--
+
+
+--Design a NONCLUSTERED INDEX with AT LEAST ONE KEY COLUMN and AT LEAST ONE INCLUDED COLUMN that improves the performance of one of the above queries--
+
